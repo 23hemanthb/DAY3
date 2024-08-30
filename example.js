52013303d4ee1760654f1bc860b04624474a71d7
unknown <bhattu123hemanth@gmail.com>
2024-08-30 09:53:06 -0700

    puched

---
+++ example.js
@@ -0,0 +1,43 @@
+let x=9;
+console.log(x);
+let name="chinna";
+console.log(name)
+const y="hi frnds"
+console.log(y);
+
+const a="HELLO WORLD"
+console.log(y)
+const isActive=true
+// console.jog(person);
+let numbers=[1,3,4,5,2]
+console.log(numbers)
+
+//functions in javascript
+
+function  WebGL2RenderingContext(){
+       console.log("function called")
+}
+
+
+//calling with a parameter
+hello("Raman");
+
+
+//declaring a function
+Comment Code
+ function a(){
+    console.log("function called")
+ }
+
+
+
+// calling  the a function
+a();
+
+//Function Expression
+const showDetails = function(){
+     console.log ("user details")
+}
+
+showDetails();
+
